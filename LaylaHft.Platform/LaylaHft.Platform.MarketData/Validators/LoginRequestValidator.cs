eba321f6c00b1e21e8353c91893199eabd874019
marielle <mbruchet@live.fr>
2025-07-24 17:43:23 +0200

    add validation rules for login and query

---
+++ LaylaHft.Platform/LaylaHft.Platform.MarketData/Validators/LoginRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using LaylaHft.Platform.Shared;
+
+namespace LaylaHft.Platform.MarketData.Validators;
+
+public class LoginRequestValidator : Validator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.ClientId)
+            .NotEmpty()
+            .WithMessage("ClientId is required.");
+
+        RuleFor(x => x.ClientSecret)
+            .NotEmpty()
+            .WithMessage("ClientSecret is required.");
+
+        RuleFor(x => x.Scopes)
+            .NotNull()
+            .WithMessage("Scopes are required.")
+            .Must(scopes => scopes.Length > 0)
+            .WithMessage("At least one scope must be provided.");
+    }
+}
