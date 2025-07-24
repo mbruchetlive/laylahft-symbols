9f76a0e2776dd452893f518ebfc5340b73495871
marielle <mbruchet@live.fr>
2025-07-24 17:24:28 +0200

    mise en place de la sécurité pour l'endpoint symbol
    ajout du token provider, du service, de l'authentification et du test d'intégration

---
+++ LaylaHft.Platform/LaylaHft.Platform.MarketData/UserLoginEndpoint.cs
@@ -0,0 +1,63 @@
+﻿using FastEndpoints;
+using LaylaHft.Platform.Shared;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LaylaHft.Platform.MarketData;
+
+public class UserLoginEndpoint : Endpoint<LoginRequest>
+{
+    public IMyAuthService MyAuthService { get; set; }
+    public IConfiguration Configuration { get; set; }
+
+    public override void Configure()
+    {
+        Post("/api/login");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(LoginRequest req, CancellationToken ct)
+    {
+        var jwtKey = Configuration["Jwt:SigningKey"];
+        ArgumentException.ThrowIfNullOrEmpty(jwtKey, "Jwt:SigningKey is null or empty");
+
+        if (await MyAuthService.CredentialsAreValid(req.ClientId, req.ClientSecret, ct))
+        {
+            var jwtToken = GenerateJwtToken(jwtKey, req.ClientId, "001", req.Scopes);
+
+            await Send.OkAsync(
+                new LoginResponse
+                {
+                    ClientId = req.ClientId,
+                    Token = jwtToken
+                });
+        }
+        else
+            ThrowError("The supplied credentials are invalid!");
+    }
+
+    public static string GenerateJwtToken(string jwtKey, string email, string userId, string[] permissions)
+    {
+        var key = Encoding.UTF8.GetBytes(jwtKey);
+        var claims = new List<Claim>
+    {
+        new Claim("UserName", email),
+        new Claim("UserId", userId),
+        new Claim("scope", string.Join(' ', permissions))
+    };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+}
---
+++ LaylaHft.Platform/LaylaHft.Platform.Shared/LoginResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace LaylaHft.Platform.Shared;
+
+public class LoginResponse
+{
+    public string ClientId { get; set; }
+    public string Token { get; set; }
+}
